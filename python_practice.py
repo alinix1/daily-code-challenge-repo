0e62f7374b7585d01de2fab94bdca2d0389c5c4c
Ali <anix1@kent.edu>
2023-05-24 14:04:03 -0600

    day 1 python practice

---
+++ python_practice.py
@@ -0,0 +1,6 @@
+def abbrev_name(name):
+   name_upper_case = name.upper()
+   result = [word[0] for word in name_upper_case.split()]
+   return result[0] + '.' + result[1]
+
+print(abbrev_name('sam smith'))
