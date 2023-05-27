000ff4d2418b7e01fe96008e6a6b261679ccbf2f
Ali <anix1@kent.edu>
2023-05-27 00:04:43 -0600

    day 2 python and javascript

--- python_solutions.py
+++ python_solutions.py
@@ -9,3 +9,23 @@ def greet(name):
     return f'Hello, {name} how are you doing today?'
 
 print(greet('Ryan'))
+
+def opposite(number):
+  if number < 0: 
+    return abs(number)
+  elif number == 0:
+    return 0
+  else: 
+    return -number
+
+print(opposite(7))
+
+def validate_hello(greetings):
+  result = greetings.lower()
+  list = ['hello', 'ciao', 'salut', 'hallo', 'hola', 'ahoj', 'czesc']
+  if any(item in result for item in list):
+    return True
+  else: 
+    return False
+
+print(validate_hello('ciao bella!'))
