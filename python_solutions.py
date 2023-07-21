a8b287cd3d4d89df8ea6029fbdef832f1c975053
Ali <anix1@kent.edu>
2023-07-20 22:26:54 -0600

    day 27 python and javascript

--- python_solutions.py
+++ python_solutions.py
@@ -1,3 +1,4 @@
+import math
 import re
 from math import sqrt
 
@@ -449,3 +450,10 @@ def open_or_senior(data):
 
 
 print(open_or_senior([(45, 12), (55, 21), (19, -2), (104, 20)]))
+
+
+def century(year):
+    return math.ceil(year / 100)
+
+
+print(century(1705))
